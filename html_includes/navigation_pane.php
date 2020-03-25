16daa25394f8cb3386c72271fd94c9006bf42eed
Adam O'Brien <17226864@studentmail.ul.ie>
2020-03-25 10:50:59 +0000

    Added Navigation Pane

--- html_includes/navigation_pane.php
+++ html_includes/navigation_pane.php
@@ -1,3 +1,9 @@
-<div class="col-lg-4">
-    Navigation Pane
+<div class="bg-light border-right col-4 text-center" id="sidebar-wrapper">
+    <div class="sidebar-heading">Start Bootstrap </div>
+    <div class="list-group list-group-flush">
+        <a href="<?php echo $_ENV['site_home'] ?>" class="list-group-item list-group-item-action bg-light">Home</a>
+        <a href="<?php echo $_ENV['site_home'] ?>profile" class="list-group-item list-group-item-action bg-light">Profile</a>
+        <a href="<?php echo $_ENV['site_home'] ?>chat" class="list-group-item list-group-item-action bg-light">Chat</a>
+        <?php if($_SESSION['admin'] == 1){ echo '<a href="{' . $_ENV['site_home'] . '/admin" class="list-group-item list-group-item-action bg-light">Admin</a>'; } ?>
+    </div>
 </div>
--- profile/index.php
+++ profile/index.php
@@ -49,6 +49,8 @@ $user_traits = get_user_traits($db_conn);
 
 	<link rel="stylesheet" href="profile.css">
 
+	<link rel="stylesheet" href="../html_includes/css/navigation_sidebar.css"/>
+
 	<script src="https://code.jquery.com/jquery-1.12.4.js"></script>
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
