0e0e7aab951cbfb191ad821869337ead4249182f
mcnabola <17226163@studentmail.ul.ie>
2020-03-31 02:15:58 +0100

    Added nav bar to the edit profile page - the nav bar does work. THe problem is the Jquery for the interest and traits do not work. There is a index_old that has the full functionality as before

---
+++ navbar.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+
+
+<body>
+<div id="wrapper">
+    <div id="sidebar-wrapper">
+        <ul class="sidebar-nav">
+            <li class="sidebar-brand"><a href="#"><img src="https://cdn.pixabay.com/photo/2017/06/13/12/53/profile-2398782_960_720.png" width="50px" height="50px" alt="Icon" />&nbsp;Mark McNabola</a></li>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">SEARCH </a></li>
+            <li><a href="#">CHAT </a></li>
+            <li><a href="#">PROFILE </a></li>
+            <li><a style="color:pink" href="#">ADMIN </a></li>
+        </ul>
+    </div>
+
+</body>
+
+</html>
