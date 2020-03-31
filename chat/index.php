4f8fe30eda96e07e588f558345b79590a0ba2039
mcnabola <17226163@studentmail.ul.ie>
2020-03-31 18:22:16 +0100

    Updated navbar with more fixes to work on chat and admin page

--- chat/index.php
+++ chat/index.php
@@ -20,32 +20,37 @@ login_check(1);
 
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
-        <link rel="stylesheet" href="..\assets/bootstrap/css/bootstrap.min.css">
-        <link rel="stylesheet" href="..\assets/fonts/font-awesome.min.css">
+
         <link rel="stylesheet" href="..\assets/css/Sidebar-Menu-1.css">
         <link rel="stylesheet" href="..\assets/css/Sidebar-Menu.css">
         <link rel="stylesheet" href="..\assets/css/styles.css">
+
+
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css">
+
+
+        <script src="https://code.jquery.com/jquery-1.12.4.js"></script>
+        <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
 
 
 
         <link rel="stylesheet" href="chat.css" />
 
 
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-   
     </head>
 
 
 <body>
 
-<?php include ('..\navbar.php'); ?>
+<div id="wrapper">
+    <?php include('..\navbar.php'); ?>
 
-<div class="page-content-wrapper">
-    <div class="container-fluid"><a class="btn btn-link" role="button" id="menu-toggle" href="#menu-toggle"><i class="fa fa-bars"></i> MENU</a>
+    <div class="page-content-wrapper">
+        <div class="container-fluid">
+            <a class="btn btn-link" role="button" id="menu-toggle" href="#menu-toggle"><i class="fa fa-bars"></i> MENU</a>
 <div class="bootstrap_chat">
 <div class="container py-5 px-4">
   <!-- For demo purpose-->
@@ -239,10 +244,10 @@ login_check(1);
 
     </div>
 
-    <script src="..\assets/js/jquery.min.js"></script>
-    <script src="..\assets/bootstrap/js/bootstrap.min.js"></script>
-    <script src="..\assets/js/Sidebar-Menu.js"></script>
+    </div>
 </div>
+
+<script src="..\assets/js/Sidebar-Menu.js"></script>
 
 
 
