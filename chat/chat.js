e41ac8c6cd1cfe7c8433a4298a9987415c19a8fe
Adam O'Brien <17226864@studentmail.ul.ie>
2020-04-15 12:17:12 +0100

    Added new message notifications in chat box

--- chat/chat.js
+++ chat/chat.js
@@ -67,24 +67,23 @@ function displayMessages(data, index) {
     mBox.empty();
 
     jQuery.each(data, function (i, val) {
-        var cls = "btn-outline-primary";
+        var cls = "";
 
         var element = "";
         var messageWhen = moment(val.timestamp, "YYYY-MM-DD hh:ii:ss").from(moment(new Date()));
 
         if (val.user_id == chats[index].other_user_id) {
             // This message is from the other user
-            cls = "btn-outline-danger";
 
             if (chats[index].you_are_user == "A") {
                 if (val.timestamp > chats[index].A_last_viewed) {
                     // This is a new unread message
-                    cls = "btn-success";
+                    cls = "<sup><span class='badge badge-success'>1</span></sup>";
                 }
             } else {
                 if (val.timestamp > chats[index].B_last_viewed) {
                     // This is a new unread message
-                    cls = "btn-success";
+                    cls = "<sup><span class='badge badge-success'>1</span></sup>";
                 }
             }
 
@@ -93,6 +92,7 @@ function displayMessages(data, index) {
                     <div class="received_msg">
                     <div class="received_withd_msg">
                         <p>${val.message}</p>
+                        ${cls}
                         <span class="time_date">${messageWhen}</span></div>
                     </div>
                 </div>
@@ -105,6 +105,7 @@ function displayMessages(data, index) {
                 <div class="outgoing_msg">
                     <div class="sent_msg">
                     <p>${val.message}</p>
+                    ${cls}
                     <span class="time_date">${messageWhen}</span> </div>
                 </div>
             `;
